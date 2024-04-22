ff6cf2e2087199c3b3f03efcb556b06255e6964a
Sergey-Ivanovich <ericnoata@gmail.com>
2024-04-22 19:44:20 +0100

    alot of work on toggleable navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+let navToggle = document.querySelector(".toggle-nav");
+let nav = document.querySelector(".nav-container");
 
+function toggleNavbar() {
+  nav.classList.toggle("hide-bar");
+}
+
+navToggle.addEventListener("click", toggleNavbar);
+
+function hideHintText() {
+  hintText.classList.add("hide");
+}
+
+let hintText = document.querySelector("#hint-navbar");
+
+setTimeout(hideHintText, 5000);
